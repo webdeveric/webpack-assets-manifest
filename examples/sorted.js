304646ce874b48585d48a2285ee85ae50ca1149e
Eric King <eric@webdeveric.com>
2025-02-09 19:40:18 -0600

    chore: simplified `sortManifest` example.

--- examples/sorted.js
+++ examples/sorted.js
@@ -4,17 +4,7 @@ const manifest = new WebpackAssetsManifest({
   output: 'sorted-manifest.json',
   sortManifest(a, b) {
     // `this` is the manifest instance.
-    const extA = this.getExtension(a);
-    const extB = this.getExtension(b);
 
-    if (extA > extB) {
-      return 1;
-    }
-
-    if (extA < extB) {
-      return -1;
-    }
-
-    return a.localeCompare(b);
+    return this.getExtension(a).localeCompare(this.getExtension(b)) || a.localeCompare(b);
   },
 });
